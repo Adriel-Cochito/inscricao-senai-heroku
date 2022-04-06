a4713139bae338e4c40df735ea977ffdeb90e028
Adriel Cochito <adriel.cochito@al.infnet.edu.br>
2022-04-06 01:54:30 -0300

    reset

--- bin/src/main/resources/static/js/candidato.js
+++
@@ -1,9 +0,0 @@
-//	$('#perfis').ready(function(){
-//		var perfis = [[${usuario.perfis}]];
-//		$.each(perfis, function(k, v) {
-//			$('#perfis option[value="'+ v.id +'"]').attr('selected', 'selected');
-//		})
-//	});
-//	
-//	
-//	console.log("candidato.js executado!!!")
