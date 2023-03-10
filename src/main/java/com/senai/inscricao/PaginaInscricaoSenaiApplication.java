dd2028c3aecfddcba866ca293e6cabe973e077eb
Adriel Cochito <adriel.cochito@al.infnet.edu.br>
2023-03-09 22:11:09 -0300

    NEW - Correcao admin

--- src/main/java/com/senai/inscricao/PaginaInscricaoSenaiApplication.java
+++ src/main/java/com/senai/inscricao/PaginaInscricaoSenaiApplication.java
@@ -1,15 +1,46 @@
 package com.senai.inscricao;
 
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.senai.inscricao.domains.Perfil;
+import com.senai.inscricao.domains.Usuario;
+import com.senai.inscricao.services.UsuarioService;
 
 
 @SpringBootApplication
 public class PaginaInscricaoSenaiApplication  {
-
+	
+	@Autowired
+	private static UsuarioService usuarioService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PaginaInscricaoSenaiApplication.class, args);
+		
+		Usuario usuario = new Usuario();
+		List<Perfil> perfil = Arrays.asList(new Perfil(1L), new Perfil(2L));
+		LocalDate dataAtual = LocalDate.now();
+		usuario.setPerfis(perfil);
+		usuario.setAtivo(true);
+		usuario.setCpf("12345678901");
+		usuario.setEmail("123@123");
+		usuario.setDtInscricao(dataAtual);
+		usuario.setSenha("12345678901");
+		usuario.setStatusCadastro(0);	
+		try {
+			usuarioService.salvarUsuario(usuario);
+		} catch (Exception e) {
+			System.out.println("Erro ao criar admin");
+		}
+		
+			
+			
 	} 
 	
 }
